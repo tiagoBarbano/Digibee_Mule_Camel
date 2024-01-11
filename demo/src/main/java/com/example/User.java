6c0c26295888e2d2c6366dfe7294faa73e09294d
tiagoBarbano <tiago.barbano@gmail.com>
2024-01-11 13:13:59 -0300

    init

---
+++ demo/src/main/java/com/example/User.java
@@ -0,0 +1,44 @@
+package com.example;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+/**
+ * User entity
+ *
+ */
+public class User {
+
+    private Integer id;
+
+    private String name;
+
+    public User() {
+    }
+
+    public User(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
+
+}
